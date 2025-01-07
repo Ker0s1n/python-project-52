8e112121593d4a14adc34ee39317e837c0cc3998
HomePCwsl <chachimin@yandex.ru>
2025-01-07 13:46:35 +0300

    update users/admin.py

---
+++ task_manager/users/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+# Unregister the provided model admin
+admin.site.unregister(get_user_model())
+
+
+# Register out own model admin, based on the default UserAdmin
+@admin.register(get_user_model())
+class CustomUserAdmin(UserAdmin):
+    list_display = [
+        "username",
+        "email",
+        "last_name",
+        "first_name",
+        "last_login",
+        "date_joined",
+    ]
+    search_fields = [
+        "username",
+        "email" "last_name",
+        "first_name",
+    ]
