c8e045db425224125aa576252e519b0aaefecfe3
HomePCwsl <chachimin@yandex.ru>
2025-01-12 22:42:47 +0300

    step 5 model forming

---
+++ task_manager/tasks/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from task_manager.statuses.models import Status
+
+User = get_user_model()
+
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=255, blank=False, unique=True, verbose_name=_("Name")
+    )
+    body = models.TextField(blank=False, verbose_name=_("Description"))
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name=_("Author"),
+        related_name=_("Author"),
+    )
+    status = models.ForeignKey(
+        Status, on_delete=models.PROTECT, verbose_name=_("Status")
+    )
+    perfomer = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name=_("Perfomer"),
+        related_name=_("Perfomer"),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ task_manager/tasks/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from task_manager.tasks.views import (
+    TaskCreateView,
+    TaskDeleteView,
+    TaskInfoView,
+    TaskListView,
+    TaskUpdateView,
+)
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="tasks_list"),
+    path("create/", TaskCreateView.as_view(), name="task_create"),
+    path("<int:pk>/", TaskInfoView.as_view(), name="tasks_info"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
+]
