23af4e232aefd4f1c60416b7c57e282b9b5063b2
Work Chachimin <chachimin@yandex.ru>
2025-01-20 12:19:42 +0300

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["webserver", "127.0.0.1"]
+ALLOWED_HOSTS = ["webserver", "127.0.0.1", "task-manager-ch.onrender.com"]
 
 
 # Application definition
